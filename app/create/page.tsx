b9273876509409e23e689b1226f680436f3efe7c
Klaudia Biala <klaudiabiaa@gmail.com>
2024-04-23 14:21:03 +0200

    Add  Theme to layout and delete from oder files

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import { Button, Stack, ThemeProvider, TextField, Autocomplete } from "@mui/material";
-import theme from "../theme";
-import "../mainpage.css";
 import Link from "next/link";
 
 export default function Create() {
@@ -23,81 +21,77 @@ export default function Create() {
 	};
 
 	return (
-		<>
-			<ThemeProvider theme={theme}>
-				<form onSubmit={handleSubmit}>
-					<Stack spacing={2} justifyContent="center" alignItems="center">
-						<h1 className="pageTitle" style={{ width: "100%" }}>
-							Create Game
-						</h1>
-						<Link href="/">
-							<Button
-								variant="contained"
-								size="small"
-								sx={{
-									bgcolor: "main",
-									":hover": { backgroundColor: "dark" },
-								}}
-							>
-								Back
-							</Button>
-						</Link>
-						<TextField
-							InputLabelProps={{ htmlFor: "outlined-multiline-flexible" }}
-							name="game_name"
-							id="outlined-multiline-flexible"
-							label="Game Name"
-							sx={{ width: "70%" }}
-						/>
-						<Autocomplete
-							disablePortal
-							options={typeOfGames}
-							sx={{ width: "70%" }}
-							renderInput={(params) => (
-								<TextField {...params} label="Type Of Game" name="type_of_game" />
-							)}
-						/>
-						<Autocomplete
-							disablePortal
-							options={owned}
-							sx={{ width: "70%" }}
-							renderInput={(params) => <TextField {...params} label="Owned?" name="owned" />}
-						/>
-						<Autocomplete
-							disablePortal
-							options={status}
-							sx={{ width: "70%" }}
-							renderInput={(params) => <TextField {...params} label="Status" name="status" />}
-						/>
+		<form onSubmit={handleSubmit}>
+			<Stack spacing={2} justifyContent="center" alignItems="center">
+				<h1 className="pageTitle" style={{ width: "100%" }}>
+					Create Game
+				</h1>
+				<Link href="/">
+					<Button
+						variant="contained"
+						size="small"
+						sx={{
+							bgcolor: "main",
+							":hover": { backgroundColor: "dark" },
+						}}
+					>
+						Back
+					</Button>
+				</Link>
+				<TextField
+					InputLabelProps={{ htmlFor: "outlined-multiline-flexible" }}
+					name="game_name"
+					id="outlined-multiline-flexible"
+					label="Game Name"
+					sx={{ width: "70%" }}
+				/>
+				<Autocomplete
+					disablePortal
+					options={typeOfGames}
+					sx={{ width: "70%" }}
+					renderInput={(params) => (
+						<TextField {...params} label="Type Of Game" name="type_of_game" />
+					)}
+				/>
+				<Autocomplete
+					disablePortal
+					options={owned}
+					sx={{ width: "70%" }}
+					renderInput={(params) => <TextField {...params} label="Owned?" name="owned" />}
+				/>
+				<Autocomplete
+					disablePortal
+					options={status}
+					sx={{ width: "70%" }}
+					renderInput={(params) => <TextField {...params} label="Status" name="status" />}
+				/>
 
-						<Autocomplete
-							multiple
-							id="tags-outlined"
-							options={platform}
-							sx={{ width: "70%" }}
-							getOptionLabel={(option) => option}
-							filterSelectedOptions
-							renderInput={(params) => (
-								<TextField {...params} label="Platform" placeholder="Platform" name="platform" />
-							)}
-						/>
-						<Button
-							type="submit"
-							variant="contained"
-							size="large"
-							sx={{
-								bgcolor: "main",
-								":hover": { backgroundColor: "dark" },
+				<Autocomplete
+					multiple
+					id="tags-outlined"
+					options={platform}
+					sx={{ width: "70%" }}
+					getOptionLabel={(option) => option}
+					filterSelectedOptions
+					renderInput={(params) => (
+						<TextField {...params} label="Platform" placeholder="Platform" name="platform" />
+					)}
+				/>
+				<Button
+					type="submit"
+					variant="contained"
+					size="large"
+					sx={{
+						bgcolor: "main",
+						":hover": { backgroundColor: "dark" },
 
-								margin: "5px",
-							}}
-						>
-							Create
-						</Button>
-					</Stack>
-				</form>
-			</ThemeProvider>
-		</>
+						margin: "5px",
+					}}
+				>
+					Create
+				</Button>
+			</Stack>
+		</form>
 	);
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,38 @@
 "use client";
-import "./mainpage.css";
+
 import { Button } from "@mui/material";
 import Link from "next/link";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
+import ReactVirtualizedTable from "@/components/GameTable";
 
 export default function MainPage() {
+	async function showGames() {
+		const response = await fetch("api/games");
+		const games = await response.json();
+		return games;
+	}
+	showGames();
+
 	return (
 		<>
-			<ThemeProvider theme={theme}>
-				<h1 className="pageTitle" style={{ marginBlockStart: "0%" }}>
-					Games Progress Tracker
-				</h1>
-				<Link href="/create">
-					<Button
-						variant="contained"
-						size="large"
-						sx={{
-							bgcolor: "main",
-							":hover": { backgroundColor: "dark" },
-							position: "absolute",
-							left: "50%",
-							transform: "translate(-50%,0)",
-						}}
-					>
-						Add Game
-					</Button>
-				</Link>
-			</ThemeProvider>
+			<h1 className="pageTitle" style={{ marginBlockStart: "0%" }}>
+				Games Progress Tracker
+			</h1>
+			<Link href="/create">
+				<Button
+					variant="contained"
+					size="large"
+					sx={{
+						bgcolor: "main",
+						":hover": { backgroundColor: "dark" },
+						position: "absolute",
+						left: "50%",
+						transform: "translate(-50%,0)",
+					}}
+				>
+					Add Game
+				</Button>
+				{/* {ReactVirtualizedTable()} */}
+			</Link>
 		</>
 	);
 }
