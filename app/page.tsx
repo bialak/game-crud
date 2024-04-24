2e357c58819a0df825798841fdd34eca4fe0d0a9
Klaudia Biala <klaudiabiaa@gmail.com>
2024-04-24 14:29:59 +0200

    undo changes to another PR

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
 "use client";
 import { Button } from "@mui/material";
 import Link from "next/link";
-import ReactVirtualizedTable from "@/components/GameTable";
 
 export default function MainPage() {
-	async function showGames() {
-		const response = await fetch("api/games");
-		const games = await response.json();
-		return games;
-	}
-	showGames();
-
 	return (
 		<>
 			<h1 className="pageTitle" style={{ marginBlockStart: "0%" }}>
@@ -30,7 +22,6 @@ export default function MainPage() {
 				>
 					Add Game
 				</Button>
-				{/* {ReactVirtualizedTable()} */}
 			</Link>
 		</>
 	);
