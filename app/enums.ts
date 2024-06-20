4d8d3acbc4db50c22da58c8bc1531e14404ee76c
Klaudia Biala <klaudiabiaa@gmail.com>
2024-06-20 11:52:05 +0200

    create enums.ts

---
+++ app/enums.ts
@@ -0,0 +1,12 @@
+export enum OwnedOptions {
+	Yes = "TRUE",
+	No = "FALSE",
+}
+
+export enum StatusOptions {
+	PLAYING = "Playing",
+	FINISHED = "Finished",
+	ONHOLD = "On hold",
+	ABONDEND = "Abondend",
+	PLANING = "Planing",
+}
