58d1ba09990e69c369981737f85e78a65029bd79
bialak <klauiabiaa@gmail.com>
2024-07-01 13:53:14 +0200

    change route.tsx to show a good status

--- app/api/games/route.tsx
+++ app/api/games/route.tsx
@@ -1,23 +1,24 @@
 import prisma from "@/app/api/games/validation";
 
-export async function POST(req: Request) {
-  const body = await req.json();
+export async function POST(request: Request) {
+  const body = await request.json();
 
-  const result = await prisma.game.create({
-    data: {
-      ...body,
-    },
-  });
-  if (!result)
-    return Response.json({
-      message: "error",
-      status: 500,
+  try {
+    const result = await prisma.game.create({
+      data: {
+        ...body,
+      },
     });
 
-  return Response.json({ message: "ok", status: 200, data: result });
+    return new Response(JSON.stringify({ ...result }), { status: 201 });
+  } catch (e) {
+    return new Response(JSON.stringify({ message: "Validation error" }), {
+      status: 400,
+    });
+  }
 }
 
 export async function GET(req: Request) {
   const result = await prisma.game.findMany();
-  return Response.json({ message: "ok", status: 200, data: result });
+  return Response.json({ message: "ok", status: 201, data: result });
 }
