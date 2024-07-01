f6579f3bd11dc8889bcc0f9b633e753b34e3f35a
bialak <klauiabiaa@gmail.com>
2024-07-01 12:36:49 +0200

    change path of prisma

--- app/api/games/route.tsx
+++ app/api/games/route.tsx
@@ -1,22 +1,23 @@
-import prisma from "@/prisma/prisma";
+import prisma from "@/app/api/games/validation";
 
 export async function POST(req: Request) {
-	const body = await req.json();
+  const body = await req.json();
 
-	const result = await prisma.game.create({
-		data: {
-			...body,
-		},
-	});
-	if (!result)
-		return Response.json({
-			message: "error",
-			status: 500,
-		});
-	return Response.json({ message: "ok", status: 200, data: result });
+  const result = await prisma.game.create({
+    data: {
+      ...body,
+    },
+  });
+  if (!result)
+    return Response.json({
+      message: "error",
+      status: 500,
+    });
+
+  return Response.json({ message: "ok", status: 200, data: result });
 }
 
 export async function GET(req: Request) {
-	const result = await prisma.game.findMany();
-	return Response.json({ message: "ok", status: 200, data: result });
+  const result = await prisma.game.findMany();
+  return Response.json({ message: "ok", status: 200, data: result });
 }
