a2b7d7378ae41873207034b9f3b31ff37e8a05bc
bialak <klauiabiaa@gmail.com>
2024-07-12 17:41:28 +0200

    deleted redundant code

--- app/api/games/validation.tsx
+++ app/api/games/validation.tsx
@@ -1,5 +1,5 @@
 import { PrismaClient, Prisma } from "@prisma/client";
-import * as Yup from "yup";
+
 import { validationSchema } from "@/validation/validationSchema";
 
 const prisma = new PrismaClient().$extends({
@@ -11,19 +11,6 @@ const prisma = new PrismaClient().$extends({
       },
       update({ args, query }) {
         validationSchema.validateSync(args.data, { abortEarly: false });
-        return query(args);
-      },
-      updateMany({ args, query }) {
-        validationSchema.validateSync(args.data, { abortEarly: false });
-        return query(args);
-      },
-      upsert({ args, query }) {
-        validationSchema.validateSync(args.create, {
-          abortEarly: false,
-        });
-        validationSchema.validateSync(args.update, {
-          abortEarly: false,
-        });
         return query(args);
       },
     },
