3df2c7d3fc9b362c86f26352ad830e4c1d2df01e
bialak <klauiabiaa@gmail.com>
2024-07-15 14:04:54 +0200

    Remove unnecessary react fragment

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -3,47 +3,45 @@ import { Controller, Control, FieldPath } from "react-hook-form";
 import { TextField, TextFieldProps } from "@mui/material";
 
 interface FormInputProps<T extends Record<string, any>> {
-	name: FieldPath<T>;
-	label: string;
-	variant?: "standard" | "outlined" | "filled" | undefined;
-	errorMessage?: string | undefined;
-	type?: React.InputHTMLAttributes<unknown>["type"];
-	control: Control<T>;
-	required: TextFieldProps["required"];
+  name: FieldPath<T>;
+  label: string;
+  variant?: "standard" | "outlined" | "filled" | undefined;
+  errorMessage?: string | undefined;
+  type?: React.InputHTMLAttributes<unknown>["type"];
+  control: Control<T>;
+  required: TextFieldProps["required"];
 }
 
 export const FormInput = <T extends Record<string, any>>({
-	control,
-	name,
-	variant,
-	type = "text",
-	label,
-	required,
-	errorMessage,
-	...others
+  control,
+  name,
+  variant,
+  type = "text",
+  label,
+  required,
+  errorMessage,
+  ...others
 }: FormInputProps<T>) => {
-	return (
-		<>
-			<Controller
-				name={name}
-				control={control}
-				render={({ field }) => (
-					<TextField
-						label={label}
-						type={type}
-						id="outlined-multiline-flexible"
-						sx={{ width: "70%" }}
-						variant={variant || "outlined"}
-						required={required}
-						error={Boolean(errorMessage)}
-						helperText={errorMessage}
-						{...field}
-						{...others}
-					/>
-				)}
-			/>
-		</>
-	);
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <TextField
+          label={label}
+          type={type}
+          id="outlined-multiline-flexible"
+          sx={{ width: "70%" }}
+          variant={variant || "outlined"}
+          required={required}
+          error={Boolean(errorMessage)}
+          helperText={errorMessage}
+          {...field}
+          {...others}
+        />
+      )}
+    />
+  );
 };
 
 export default FormInput;
