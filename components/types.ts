90b314e3dab14e0d632752596810ea3c317077a6
bialak <klauiabiaa@gmail.com>
2024-07-15 14:00:31 +0200

    Delete redundant tyoe

--- components/types.ts
+++ components/types.ts
@@ -1,14 +1,5 @@
 import { FieldError, UseFormRegister } from "react-hook-form";
 
-export type FormData = {
-  // id: number;
-  game_name: string;
-  type_of_game: string;
-  owned: string;
-  status: string;
-  platform: string;
-};
-
 export type FormFieldProps = {
   type: string;
   placeholder: string;
