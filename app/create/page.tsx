c6052ac3ebbd5923c2f9f6b788fd73f390af76f1
bialak <klauiabiaa@gmail.com>
2024-08-01 13:51:06 +0200

    Change name of component To CreatGamePage

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,11 +1,11 @@
-import GameForm from "@/components/GameForm";
 import React from "react";
+import CreateGameForm from "@/components/CreateGameForm";
 
 export const metadata = {
   title: "Create game tracking",
   description: "Here you can add a tracking for your game",
 };
 
-export default function GameCreate() {
-  return <GameForm />;
+export default function CreateGamePage() {
+  return <CreateGameForm />;
 }
