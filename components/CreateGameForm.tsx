5c0b3fae1b3b6987aa04424ac93e78b10e58ae2a
bialak <klauiabiaa@gmail.com>
2024-08-01 13:50:09 +0200

    Add CreateGameForm and EditGameForm which are using GameForm

---
+++ components/CreateGameForm.tsx
@@ -0,0 +1,23 @@
+"use client";
+import GameForm from "@/components/GameForm";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+export default function CreateGameForm() {
+  const router = useRouter();
+
+  function createGame(data) {
+    fetch("api/games", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then(() => {
+        router.push("/");
+      });
+  }
+  return <GameForm title="Create" onSubmit={createGame} />;
+}
---
+++ components/EditGameForm.tsx
@@ -0,0 +1,27 @@
+"use client";
+import GameForm from "@/components/GameForm";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+interface EditGameFormProps {
+  gameId: number;
+}
+
+export default function EditGameForm({ gameId }: EditGameFormProps) {
+  const router = useRouter();
+
+  function editGame(data) {
+    fetch(`api/games/${gameId}`, {
+      method: "PATCH",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then(() => {
+        router.push("/");
+      });
+  }
+  return <GameForm title="Edit" gameId={gameId} onSubmit={editGame} />;
+}
