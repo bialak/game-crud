0825e8e20926c4ea76ba9e9d20d692189fd85509
bialak <klauiabiaa@gmail.com>
2024-08-05 18:43:47 +0200

    remove redundat patch

--- app/api/games/route.tsx
+++ app/api/games/route.tsx
@@ -49,30 +49,3 @@ export async function DELETE(request: Request) {
     });
   }
 }
-export async function PATCH(request: Request) {
-  const url = new URL(request.url);
-  const id = url.pathname.split("/").pop();
-
-  if (!id || isNaN(Number(id))) {
-    return new Response(JSON.stringify({ message: "Invalid ID" }), {
-      status: 400,
-    });
-  }
-
-  const body = await request.json();
-
-  try {
-    const result = await prisma.game.update({
-      where: { id: Number(id) },
-      data: {
-        ...body,
-      },
-    });
-
-    return new Response(JSON.stringify({ result }), { status: 200 });
-  } catch (e) {
-    return new Response(JSON.stringify({ message: "Update failed" }), {
-      status: 400,
-    });
-  }
-}
